e3d10bbb80be60501466ee24c20b05be4b9c316f
paola <paolacornejo1979@gmail.com>
2022-11-12 20:04:49 -0300

    f2

--- Lector.java
+++ Lector.java
@@ -2,10 +2,7 @@
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
--- Operaciones.java
+++ Operaciones.java
@@ -1,8 +1,12 @@
 // PAOLA ANDREA CORNEJO 27671905 //
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
