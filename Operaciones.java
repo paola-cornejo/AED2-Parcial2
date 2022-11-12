fe60b3e2c7524d910f74d208af63f62103eaa6ea
paola <paolacornejo1979@gmail.com>
2022-11-12 16:15:25 -0300

    subida

--- Operaciones.java
+++ Operaciones.java
@@ -3,7 +3,7 @@ public class Operaciones {
     public static void main (String [ ] args) { 
 
         //Aquí las instrucciones del método
-        System.out.println("hola");
+        System.out.println("Probar aca");
     }
 
     
